05edb4a7899a1ba137e3064f8747a7f98b44e8d2
pnoker <peter-no@foxmail.com>
2017-08-11 18:21:54 +0800

    提交创建wireless_data表的工具方法

--- src/com/dbp/util/SpiltDataTool.java
+++ src/com/dbp/util/SpiltDataTool.java
@@ -20,18 +20,19 @@ public class SpiltDataTool {
 		SimpleDateFormat sdfOfTimeNode = new SimpleDateFormat("yyyy-MM-dd");
 		SimpleDateFormat sdfOfTable = new SimpleDateFormat("yyyy_MM");
 		Calendar calendar = Calendar.getInstance();
-		calendar.set(2016, 7, 1);// 设置时间，一个月一个月分表
+		calendar.set(2017, 6, 1);// 设置时间，一个月一个月分表,真实月份加 1
 		Date dataOfTable = calendar.getTime();
 		calendar.add(Calendar.MONTH, 1);// 下个月,时间节点
 		Date dataOfTimeNode = calendar.getTime();
 		TableOption tableOption = new TableOption();
-		if (tableOption.createTable("shui_data_" + sdfOfTable.format(dataOfTable))) {
+		if (tableOption.createTable("wireless_data_" + sdfOfTable.format(dataOfTable))) {
 			System.out.println("create success");
 			DataOption dataOption = new DataOption();
-			boolean isSuccess = dataOption.copyData("shui_data", "shui_data_" + sdfOfTable.format(dataOfTable), sdfOfTimeNode.format(dataOfTimeNode));
+			boolean isSuccess = dataOption.copyData("wireless_data", "wireless_data_" + sdfOfTable.format(dataOfTable),
+					sdfOfTimeNode.format(dataOfTimeNode));
 			if (isSuccess) {
 				// 保留两个月的数据所以删除本月之前的数据
-				dataOption.deleteData("shui_data", sdfOfTimeNode.format(dataOfTable));
+				dataOption.deleteData("wireless_data", sdfOfTimeNode.format(dataOfTable));
 			}
 		} else {
 			System.out.println("create failure");
